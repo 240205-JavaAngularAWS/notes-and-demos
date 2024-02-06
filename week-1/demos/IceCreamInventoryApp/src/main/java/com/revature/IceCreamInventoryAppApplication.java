ceaa12ee0aa580e772d60886c46b3300439581ab
bryanserfozo <bryan.serfozo@revature.com>
2024-02-06 15:59:52 -0500

    Adding initial demo for spring boot proj

---
+++ week-1/demos/IceCreamInventoryApp/src/main/java/com/revature/IceCreamInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceCreamInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceCreamInventoryAppApplication.class, args);
+	}
+
+}
